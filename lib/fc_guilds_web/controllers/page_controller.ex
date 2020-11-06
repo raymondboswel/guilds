c6e9f743d8b541cdfdfa5d24b08b1cad7b32a493
raymondboswel <raymondboswel@gmail.com>
2020-11-06 12:35:30 +0200

    Added landing page for signed out users

--- lib/fc_guilds_web/controllers/page_controller.ex
+++ lib/fc_guilds_web/controllers/page_controller.ex
@@ -2,6 +2,13 @@ defmodule FcGuildsWeb.PageController do
   use FcGuildsWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    IO.inspect conn
+    if user = conn.assigns.current_user do
+
+      render(conn, "index.html", user: conn.assigns.current_user)
+    else
+      render(conn, "landing.html")
+    end
+
   end
 end

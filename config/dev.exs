9191473aefe5541f2f072852064498f5a7d7da17
raymondboswel <raymondboswel@gmail.com>
2020-11-30 14:29:51 +0200

    Changed port

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :fc_guilds, FcGuilds.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :fc_guilds, FcGuildsWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4100],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
